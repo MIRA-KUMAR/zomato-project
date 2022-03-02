f4a21c2fb529b0795e4b1af8082b1e147c005983
MIRA-KUMAR <mira.kumar.24@gmail.com>
2022-03-01 22:04:26 -0700

    Update read.js

--- src/schemas/dishes/read.js
+++ src/schemas/dishes/read.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = async (req, res, next) => {
     const DishModel = mongoose.model('Dish');
-    const data = await RestaurantModel.find({}).limit(2);
+    const data = await DishModel.find({}).limit(2);
 
     return res.send(data);
 }
