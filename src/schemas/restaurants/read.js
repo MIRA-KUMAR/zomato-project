1df1dcbd99d101f59540e27db927077a7c9822db
MIRA-KUMAR <mira.kumar.24@gmail.com>
2022-03-03 00:01:51 -0700

    Committing changes in restaurants

--- src/schemas/restaurants/read.js
+++ src/schemas/restaurants/read.js
@@ -2,7 +2,28 @@ const mongoose = require('mongoose');
 
 module.exports = async (req, res, next) => {
     const RestaurantModel = mongoose.model('Restaurant');
-    const data = await RestaurantModel.find({}).limit(2);
+    // const data = await RestaurantModel.find({}).limit(2);
 
-    return res.send(data);
+    const { page, limit } = req.query;
+    
+    const restaurant = await RestaurantModel.find()
+                                            .limit(limit*1)
+                                            .skip((page - 1)*limit)
+                                            .exec();
+    
+    const count = await RestaurantModel.countDocuments();
+
+        
+    
+    
+        // console.log(err.message);
+        // process.exit(1);
+
+    
+
+    return res.send({
+        restaurant,
+        totalPages: Math.ceil(count / limit),
+        currentPage: page
+      });
 }
