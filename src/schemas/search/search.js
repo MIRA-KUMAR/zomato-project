6180eb5942bdb100c81381ba221772edf968cf88
fizan2904 <fizan.nagarchi6@gmail.com>
2022-03-05 02:06:58 +0530

    search update

--- src/schemas/search/search.js
+++ src/schemas/search/search.js
@@ -1,30 +1,51 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
-    const { search } = req.body;
-    const { data } = await axios.post('http://localhost:9200/restaurant/_search', {
-        query: {
-            bool: {
-                should: [
-                    {
-                        match: {
-                            name: search
-                        }
-                    },
-                    {
-                        match: {
-                            address: search
-                        }
-                    },
-                    {
-                        match: {
-                            location: search
-                        }
-                    }
-                ]
+    try {
+        const { search } = req.body;
+
+        let query = {
+            query: {
+                query_string: {
+                    query: search
+                }
             }
         }
-    });
 
-    return res.status(200).send(data);
+        if (search === "") {
+            query = {
+                query: {
+                    match_all: {}
+                }
+            }
+        }
+
+        let { data: cusineData } = await axios.post('http://localhost:9200/cuisine/_search', query);
+        cusineData = cusineData.hits.hits.map(item => item._id); // [cusineIds]
+
+        const { data } = await axios.post('http://localhost:9200/restaurant/_search', {
+            query: {
+                bool: {
+                    should: [
+                        // { // restaurant name, location, address
+                        //     query_string: {
+                        //         query: search
+                        //     }
+                        // },
+                        cusineData
+                            ? {
+                                terms: {
+                                    'cuisine.keyword': cusineData
+                                }
+                            }
+                            : null
+                    ].filter(x => x !== null)
+                }
+            }
+        });
+
+        return res.status(200).send(data);
+    } catch (error) {
+        console.log(error.response.data)
+    }
 }
